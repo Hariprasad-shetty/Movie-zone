3cba5446330affd9b433c1cc55f7b4c37d76aca3
Hariprasad-shetty <shettyashish272@gmail.com>
2025-02-28 18:20:42 +0530

    Delete src directory

--- src/Components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer(params){
-  return(
-    <footer>
-      <h3>@ 2025 my website</h3>
-    </footer>
-  )
-}
-export default Footer
--- src/Components/Main.jsx
+++
@@ -1,9 +0,0 @@
-function Main(params){
-return(
-  <main>
-    <h2>The main context.</h2>
-    <p>The main component</p>
-  </main>
-)
-}
-export default Main
